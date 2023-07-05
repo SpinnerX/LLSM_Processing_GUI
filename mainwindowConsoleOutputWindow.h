2be9e3f6c39721a901334afa1f7af0eafaaf8dfe
SpinnerX <heraaronhotmail@yahoo.com>
2023-07-05 12:32:52 -0700

    Updaed to allow generating continuous output

--- mainwindowConsoleOutputWindow.h
+++ mainwindowConsoleOutputWindow.h
@@ -14,23 +14,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sstream>
-
-/**
- * @brief 
- * Idea for this class is to have features like the following or similar:
- * 
- * mainwindowConsoleOutputWindow.h
- * 
- * mainwindowConsoleOutputWindow* consoleWindow = new consoleWindow(QStirng("Matlab Output Window"), this);
- * consoleWindow.logMessage("This is a testing mesesage!");
- * 
- * For right now just to grab logs, and let this be utilized to show output on this window throughout the codebase.
- * Allows for flexbility uses.
- * 
- * 
- * This class is what creates the side window.
- * Which we can use throughout the codebase, if we ever want other code to simply call this header
-*/
+#include <QTextBrowser>
+#include "matlabthread.h"
 
 class mainwindowConsoleOutputWindow : public QDockWidget{
 public:
@@ -40,26 +25,21 @@ public:
 
     void uploadJobLogs(matlabOutputWindow *jobLogsOutputWindow);
 
-    /**
-     * @brief 
-     * 
-     * This just allows us to print logging from stdout and stderr onto the QDockWidget
-     * 
-     * @param msg 
-     * @param delimeter 
-     */
     void printLog(QString msg, char delimeter='\n');
     void printLogStdString(std::string msg, char delimeter='\n');
     void printLogStream(std::stringstream stream, char delimieter='\n');
 
 public slots:
     void updateTimer();
+    void printStdout(QString str);
 
 private:
-    QTextEdit* consoleEdit;
+    // QTextEdit* consoleEdit;
+    QTextBrowser* consoleEdit;
     QMutex* outputLock;
     std::stringstream buffer;
     QTimer* timer;
     std::vector<QBuffer *> buffers;
+    static int processCounter;
 };
 
