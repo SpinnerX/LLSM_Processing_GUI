e0b959022978088442ed5facfa19457e357a4cc3
aaron <aaron@aaronubuntu.myguest.virtualbox.org>
2023-07-25 12:24:13 -0700

    Updated main_test

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -3,47 +3,32 @@
 #include "ui_mainwindow.h"
 #include "../mainwindow.h"
 
-/*
-class Main_Tests : public QObject {
+// We will use this to emulate how our widgets will work, for right now
+// Then we will use Main_Tests to create test cases for our application
+
+class MainwindowModel : public QObject {
     Q_OBJECT
+
 private slots:
-
-    // Create Test Cases Here
-
-    void checkQTestConfiguredSuccessfully(){
-        QTest::addColumn<QString>("aString");
-        QTest::addColumn<int>("expected");
-
-        QTest::newRow("positive value") << "42" << 42;
-        QTest::newRow("negative value") << "-42" << -42;
-        QTest::newRow("zero") << "0" << 0;
-    }
-
-    // Basic testing if mainwindow can produce output.
-    // Wanting to check if the output being uploaded to the QDockWidget is working correctly
-    // While checking for bugs through these test cases.
-    void testBasicProcessOutput() { }
-
-    void testMainwindowUI() { }
+    
 };
-*/
 
 
-class Main_Tests : public QMainWindow{
+class Main_Tests : public QObject{
     Q_OBJECT
 public:
-    Main_Tests(QWidget* parent=nullptr) : QMainWindow(parent), ui(new Ui::MainWindow) {
-        ui->setupUi(this);
+    Main_Tests(){
+        ui = new Ui::MainWindow();
     }
 
     ~Main_Tests(){
         delete ui;
     }
 
-private:
-    void testingCaseVisibility() {}
+private slots:
 
-    // void testCaseVisibility2() {}
+    void testingCaseVisibility() {
+    }
 
     // Just testing that QTest is working successfully, and test cases are visible
     void checkQTestConfiguredSuccessfully(){
@@ -54,11 +39,8 @@ private:
         QTest::newRow("negative value") << "-42" << -42;
         QTest::newRow("zero") << "0" << 0;
 
-        QCOMPARE(3, 4);
+        // QTest::keyClicks(ui->customPatternsLineEdit, "Hi");
     }
-
-
-
 
 private:
     Ui::MainWindow* ui;
